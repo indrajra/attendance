48b1cc1c7dd7905a0a2d5d1ec014e9be74f18777
aishwa8141 <a.aish1997@gmail.com>
2020-04-16 11:46:38 +0530

    updated vars

--- sdk/vars.js
+++ sdk/vars.js
@@ -1,10 +1,10 @@
 const config = {
     "attendancePort":process.env.attendance_port || 7007,
     "keycloak": {
-        "url": process.env.keycloak_url || "http://localhost:9080", 
-        "realmName": process.env.keycloak_realmName || "PartnerRegistry",
-        "clientId": "attendance",
-        "clientSecret": process.env.keycloak_clientSecret || "ee8ad405-d10b-471d-82a3-363674edf6e4"
+        "url": process.env.keycloak_url || "http://localhost:8080", 
+        "realmName": process.env.keycloak_realmName || "TeacherRegistry",
+        "clientId": "stall4",
+        "clientSecret": process.env.keycloak_clientSecret || "a2089feb-4318-4016-b753-d0e175ca6e5f"
     },
     "utilServiceUrl":"http://localhost:9081"
 }
